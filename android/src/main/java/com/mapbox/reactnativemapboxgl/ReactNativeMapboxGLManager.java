b9555a17cbd46b4dc558d4c4a1b61047ad58133b
Leith Bade <leith@mapbox.com>
2015-10-23 20:12:03 +1100

    Upgrade to mapbox-gl-native 2.1.0 Fix a few coding errors

--- android/src/main/java/com/mapbox/reactnativemapboxgl/ReactNativeMapboxGLManager.java
+++ android/src/main/java/com/mapbox/reactnativemapboxgl/ReactNativeMapboxGLManager.java
@@ -3,7 +3,6 @@ package com.mapbox.reactnativemapboxgl;
 import android.graphics.Color;
 import android.util.Log;
 
-import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.uimanager.CatalystStylesDiffMap;
 import com.facebook.react.uimanager.SimpleViewManager;
@@ -50,8 +49,7 @@ public class ReactNativeMapboxGLManager extends SimpleViewManager<MapView> {
 
     @Override
     public MapView createViewInstance(ThemedReactContext context) {
-        MapView mv = new MapView(context);
-        mv.setAccessToken("pk.foobar"); // Placeholder access token
+        MapView mv = new MapView(context, "pk.foobar"); // Placeholder access token
         mv.onCreate(null);
         return mv;
     }
@@ -65,6 +63,7 @@ public class ReactNativeMapboxGLManager extends SimpleViewManager<MapView> {
         } else {
             view.setAccessToken(props.getString(PROP_ACCESS_TOKEN));
         }
+        /* This is not in 2.1.0 but should be in next release
         if (props.hasKey(PROP_USER_TRACKING_MODE)) {
             String mode = props.getString(PROP_USER_TRACKING_MODE);
             if (mode.equals("NONE")) {
@@ -77,73 +76,77 @@ public class ReactNativeMapboxGLManager extends SimpleViewManager<MapView> {
                 view.setUserLocationTrackingMode(MapView.UserLocationTrackingMode.NONE);
                 Log.w("Error", "Tracking mode not found. Setting to NONE.");
             }
-        }
+        }*/
         if (props.hasKey(PROP_ANNOTATIONS)) {
             int size = props.getArray(PROP_ANNOTATIONS).size();
             for (int i = 0; i < size; i++) {
                 ReadableMap annotation = props.getArray(PROP_ANNOTATIONS).getMap(i);
                 String type = annotation.getString("type");
-                if (type.equals("point")) {
-                    double latitude = annotation.getArray("coordinates").getDouble(0);
-                    double longitude = annotation.getArray("coordinates").getDouble(1);
-                    LatLng markerCenter = new LatLng(latitude, longitude);
-                    MarkerOptions marker = new MarkerOptions();
-                    marker.position(markerCenter);
-                    if (annotation.hasKey("title")) {
-                        String title = annotation.getString("title");
-                        marker.title(title);
+                double latitude;
+                double longitude;
+                switch (type) {
+                    case "point":
+                        latitude = annotation.getArray("coordinates").getDouble(0);
+                        longitude = annotation.getArray("coordinates").getDouble(1);
+                        LatLng markerCenter = new LatLng(latitude, longitude);
+                        MarkerOptions marker = new MarkerOptions();
+                        marker.position(markerCenter);
+                        if (annotation.hasKey("title")) {
+                            String title = annotation.getString("title");
+                            marker.title(title);
+                        }
+                        if (annotation.hasKey("subtitle")) {
+                            String subtitle = annotation.getString("subtitle");
+                            marker.snippet(subtitle);
+                        }
+                        view.addMarker(marker);
+                        break;
+                    case "polyline": {
+                        int coordSize = annotation.getArray("coordinates").size();
+                        PolylineOptions polyline = new PolylineOptions();
+                        for (int p = 0; p < coordSize; p++) {
+                            latitude = annotation.getArray("coordinates").getArray(p).getDouble(0);
+                            longitude = annotation.getArray("coordinates").getArray(p).getDouble(1);
+                            polyline.add(new LatLng(latitude, longitude));
+                        }
+                        if (annotation.hasKey("alpha")) {
+                            double strokeAlpha = annotation.getDouble("alpha");
+                            polyline.alpha((float) strokeAlpha);
+                        }
+                        if (annotation.hasKey("strokeColor")) {
+                            int strokeColor = Color.parseColor(annotation.getString("strokeColor"));
+                            polyline.color(strokeColor);
+                        }
+                        if (annotation.hasKey("strokeWidth")) {
+                            float strokeWidth = annotation.getInt("strokeWidth");
+                            polyline.width(strokeWidth);
+                        }
+                        view.addPolyline(polyline);
+                        break;
                     }
-                    if (annotation.hasKey("subtitle")) {
-                        String subtitle = annotation.getString("subtitle");
-                        marker.snippet(subtitle);
+                    case "polygon": {
+                        int coordSize = annotation.getArray("coordinates").size();
+                        PolygonOptions polygon = new PolygonOptions();
+                        for (int p = 0; p < coordSize; p++) {
+                            latitude = annotation.getArray("coordinates").getArray(p).getDouble(0);
+                            longitude = annotation.getArray("coordinates").getArray(p).getDouble(1);
+                            polygon.add(new LatLng(latitude, longitude));
+                        }
+                        if (annotation.hasKey("alpha")) {
+                            double fillAlpha = annotation.getDouble("alpha");
+                            polygon.alpha((float) fillAlpha);
+                        }
+                        if (annotation.hasKey("fillColor")) {
+                            int fillColor = Color.parseColor(annotation.getString("fillColor"));
+                            polygon.fillColor(fillColor);
+                        }
+                        if (annotation.hasKey("strokeColor")) {
+                            int strokeColor = Color.parseColor(annotation.getString("strokeColor"));
+                            polygon.strokeColor(strokeColor);
+                        }
+                        view.addPolygon(polygon);
+                        break;
                     }
-                    view.addMarker(marker);
-                } else if (type.equals("polyline")) {
-                    int coordSize = annotation.getArray("coordinates").size();
-                    PolylineOptions polyline = new PolylineOptions();
-                    for (int p = 0; p < coordSize; p++) {
-                        double latitude = annotation.getArray("coordinates").getArray(p).getDouble(0);
-                        double longitude = annotation.getArray("coordinates").getArray(p).getDouble(1);
-                        polyline.add(new LatLng(latitude, longitude));
-                    }
-                    if (annotation.hasKey("alpha")) {
-                        double strokeAlpha = annotation.getDouble("alpha");
-                        polyline.alpha((float) strokeAlpha);
-                    }
-                    if (annotation.hasKey("strokeColor")) {
-                        int strokeColor = Color.parseColor(annotation.getString("strokeColor"));
-                        polyline.color(strokeColor);
-                    }
-                    if (annotation.hasKey("strokeWidth")) {
-                        float strokeWidth = annotation.getInt("strokeWidth");
-                        polyline.width(strokeWidth);
-                    }
-                    view.addPolyline(polyline);
-                } else if (type.equals("polygon")) {
-                    int coordSize = annotation.getArray("coordinates").size();
-                    PolygonOptions polygon = new PolygonOptions();
-                    for (int p = 0; p < coordSize; p++) {
-                        double latitude = annotation.getArray("coordinates").getArray(p).getDouble(0);
-                        double longitude = annotation.getArray("coordinates").getArray(p).getDouble(1);
-                        polygon.add(new LatLng(latitude, longitude));
-                    }
-                    if (annotation.hasKey("alpha")) {
-                        double fillAlpha = annotation.getDouble("alpha");
-                        polygon.alpha((float) fillAlpha);
-                    }
-                    if (annotation.hasKey("fillColor")) {
-                        int fillColor = Color.parseColor(annotation.getString("fillColor"));
-                        polygon.fillColor(fillColor);
-                    }
-                    if (annotation.hasKey("strokeColor")) {
-                        int strokeColor = Color.parseColor(annotation.getString("strokeColor"));
-                        polygon.strokeColor(strokeColor);
-                    }
-                    if (annotation.hasKey("strokeWidth")) {
-                        float strokeWidth = annotation.getInt("strokeWidth");
-                        polygon.strokeWidth(strokeWidth);
-                    }
-                    view.addPolygon(polygon);
                 }
             }
         }
@@ -168,11 +171,14 @@ public class ReactNativeMapboxGLManager extends SimpleViewManager<MapView> {
         if (props.hasKey(PROP_STYLE_URL)) {
             view.setStyleUrl(props.getString(PROP_STYLE_URL));
         }
-        if (props.hasKey(PROP_STYLE_URL)) {
+        if (props.hasKey(PROP_ZOOM_ENABLED)) {
             view.setZoomEnabled(props.getBoolean(PROP_ZOOM_ENABLED, true));
         }
         if (props.hasKey(PROP_ZOOM_LEVEL)) {
             view.setZoomLevel(props.getFloat(PROP_ZOOM_LEVEL, 0));
+        }
+        if (props.hasKey(PROP_SCROLL_ENABLED)) {
+            view.setZoomEnabled(props.getBoolean(PROP_SCROLL_ENABLED, true));
         }
 
         super.updateView(view, props);
